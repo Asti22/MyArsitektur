875213556fb700befc737ccb7b595385ec31ad58
Asti22 <uun3461@gmail.com>
2025-11-20 09:41:44 +0700

    Membuat state untuk input Siswa dan listData untuk dikirim ke ViewModel.

--- app/src/androidTest/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
+++ app/src/androidTest/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
@@ -1,7 +1,15 @@
 package com.example.myarsitektur.view.uicontroller
 
+import android.R
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormSiswa(
@@ -9,5 +17,13 @@ fun FormSiswa(
     onSubmitButtonClicked:(MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var txtNama by rememberSaveable { mutableStateOf(value = "") }
+    var txtAlamat by remember {mutableStateOf(value = "")}
+    var txtGender by remember { mutableStateOf(value ="") }
+    var listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
+    scaffold(modifier = Modifier,
+        topBar = { TopAppBar(title = { Text(text = stringResource(id = "Form Pendaftaran"), color = Color.White ) }
+
+        ) })
 }
