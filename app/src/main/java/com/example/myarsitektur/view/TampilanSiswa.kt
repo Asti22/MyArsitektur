db2ae956aa6abf790a4cb2d2cec0d658a901212e
Asti22 <uun3461@gmail.com>
2025-11-20 14:08:17 +0700

    Menambahkan TopAppBar untuk halaman data siswa dengan judul dan warna latar yang sesuai tema aplikasi

--- app/src/main/java/com/example/myarsitektur/view/TampilanSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilanSiswa.kt
@@ -1,6 +1,9 @@
 package com.example.myarsitektur.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.myarsitektur.R
 import com.example.myarsitektur.model.Siswa
@@ -16,4 +19,13 @@ fun TampilSiswa(
         Pair(stringResource(id = "Jenis Kelamin"), statusUISiswa.gender),
         Pair(stringResource("Alamat"), statusUISiswa.alamat)
     )
+
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.data_siswa), color = colorResource(R.color.white)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(backgroundColor = colorResource(id = R.color.purple_500)),
+            )
+        })
+
 }
