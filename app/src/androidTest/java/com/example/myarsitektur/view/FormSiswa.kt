f69b526faa8000cbe09a9561e0dade4f23193e61
Asti22 <uun3461@gmail.com>
2025-11-20 12:39:13 +0700

    Menambahkan Scaffold dengan TopAppBar dan layout Column untuk struktur tampilan form pendaftaran

--- app/src/androidTest/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/androidTest/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -1,14 +1,19 @@
 package com.example.myarsitektur.view.uicontroller
 
 import android.R
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @Composable
@@ -23,7 +28,17 @@ fun FormSiswa(
     var listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
     scaffold(modifier = Modifier,
-        topBar = { TopAppBar(title = { Text(text = stringResource(id = "Form Pendaftaran"), color = Color.White ) }
+        topBar = { TopAppBar(title = { Text(text = stringResource(id = "Form Pendaftaran"), color = Color.White ) },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_500))
 
-        ) })
+        )
+        }){isiRuang ->
+        Column (
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arragement.SpaceBetween,
+            horizontalAligment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField()
+        }
+    }
 }
